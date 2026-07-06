6b18cd8f92d5391542637ba2278793dfc4a37fb0
Developer Shovandeb <shovandeb2004@gmail.com>
2026-07-06 19:36:04 +0530

    Delete Backend directory

--- Backend/HR/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Assessment(models.Model):
-    Id = models.AutoField(primary_key=True)
-    examName = models.CharField(max_length=150, null=False)
-    examTopic = models.CharField(max_length=100,null=False)
-    
