d5d6322b642b739c827d3602b4759b0a51d34f34
Developer Shovandeb <shovandeb2004@gmail.com>
2026-07-06 21:51:47 +0530

    Update views.py

--- Backend/HR/views.py
+++ Backend/HR/views.py
@@ -1,41 +1,10 @@
-from django.shortcuts import render
-import json
-from django.http import JsonResponse, HttpResponse
-from .models import *
-from django.views.decorators.csrf import csrf_exempt
-# Create your views here.
+ return JsonResponse({
+        "Message":"Data not upload yet",
+        "Status" : "Pending"
+    })
 
-def Dashboard(request):
-    return render(request, 'assessment.html')
-
-@csrf_exempt
-def AssessmentCreate(request):
-    modal_data = json.loads(request.body)
-    
-    if request.method != 'POST':
-        return HttpResponse("<h2>Sorry! Its not POST method</h2>")
-    else:
-        examName = modal_data.get('examName')
-        examTopic = modal_data.get('examTopic')
-        examDuration = modal_data.get('examDuration')
-        passingMarks = modal_data.get('passingMarks')
-        startExam = modal_data.get('startExam')
-        endExam =  modal_data.get('endExam')
-        description = modal_data.get('description')
-        instructions = modal_data.get('instructions')
-        
-        Assessment.objects.create(
-            examName = examName,
-            examTopic = examTopic,
-            duration = examDuration,
-            passingMarks = passingMarks,
-            startTime = startExam,
-            endTime = endExam,
-            description = description,
-            instruction = instructions
-        )
-        return JsonResponse({
-            "status" : "Done",
-            "Assessment" : examName,
-            "Message" : "Assessment Upload succesfully"
-        })
+    assessmentChoice = modal_data.get('assessmentChoice')
+    questionType = modal_data.get('questionType')
+    questionName = modal_data.get('questionName')
+    questionMarks = modal_data.get('questionMarks')
+    Answer = modal_data.get('Answer')
